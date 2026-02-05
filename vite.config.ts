a5c8a44cd6ae14b4a8dd86fdf4c243d0b6416f24
Kushagra Pratap Singh <rajputkushagra26@gmail.com>
2026-02-06 01:27:25 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: 'camelCase'
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          dexie: ['dexie']
+        }
+      }
+    }
+  },
+  server: {
+    port: 5173,
+    host: true,
+    open: true
+  },
+  preview: {
+    port: 4173,
+    host: true,
+    open: true
+  },
+
+})
